b219e7421004cfd201848f959457ee4b2b6c022b
devdezzies <boltholt17@gmail.com>
2025-02-15 23:06:10 +0700

    fix: password regex trial

--- src/middlewares/userValidation.js
+++ src/middlewares/userValidation.js
@@ -4,7 +4,7 @@ const userValidation = () => {
     return [
         body('username', 'username is required').notEmpty(),
         body('email', 'Please enter a valid email address').isEmail(),
-        body('password', 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
+        // body('password', 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/),
         (req, res, next) => {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
