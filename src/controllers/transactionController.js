35d3b73b679539a4702c090a74c98aa9db9370fb
devdezzies <boltholt17@gmail.com>
2025-02-15 19:36:22 +0700

    fix: trial for query 1

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -124,32 +124,34 @@ const getCategorizedTransactions = async (req, res) => {
         const userId = new mongoose.Types.ObjectId(req.id); // Correctly create ObjectId instance
 
         const transactions = await Transaction.aggregate([
-             // Filter by user ID
-                        {
-                            $group: {
-                                _id: {
-                                    month: { $dateToString: { format: "%Y-%m", date: "$date", timezone: "Asia/Jakarta" } },
-                                    day: { $dateToString: { format: "%Y-%m-%d", date: "$date", timezone: "Asia/Jakarta" } },
-                                },
-                                transactions: { $push: "$$ROOT" }
-                            }
-                        },
-                        {
-                            $sort: { "_id.day": -1 } // Sort by day in descending order within each month
+            { 
+            $match: { user: userId } // Filter by user ID
             },
             {
-                $group: {
-                    _id: "$_id.month",
-                    days: {
-                        $push: {
-                            day: "$_id.day",
-                            transactions: "$transactions"
-                        }
-                    }
+            $group: {
+                _id: {
+                month: { $dateToString: { format: "%Y-%m", date: "$date", timezone: "Asia/Jakarta" } },
+                day: { $dateToString: { format: "%Y-%m-%d", date: "$date", timezone: "Asia/Jakarta" } },
+                },
+                transactions: { $push: "$$ROOT" }
+            }
+            },
+            {
+            $sort: { "_id.day": -1 } // Sort by day in descending order within each month
+            },
+            {
+            $group: {
+                _id: "$_id.month",
+                days: {
+                $push: {
+                    day: "$_id.day",
+                    transactions: "$transactions"
                 }
+                }
+            }
             },
             {
-                $sort: { "_id": -1 } // Sort by month in descending order
+            $sort: { "_id": -1 } // Sort by month in descending order
             }
         ]);
 
