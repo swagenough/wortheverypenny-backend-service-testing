69b6846ccdf2a87a352c2c3933fe4221b532d210
swagenough <151718487+swagenough@users.noreply.github.com>
2025-02-24 18:52:13 +0700

    WEY 24 fix: bug email & username middleware validator

--- src/middlewares/userValidation.js
+++ src/middlewares/userValidation.js
@@ -6,24 +6,16 @@ const userValidation = () => {
         body('username', 'username is required').notEmpty(),
         body('email', 'Please enter a valid email address').isEmail(),
         body('password', 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character').matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/),
-        body('username', '⚠️ User with the same username already exists!').custom(async (username) => {
-            try {
-                const existingUser = await User.findOne({ username })
-                if (existingUser) {
-                    throw new Error()
-                }
-            } catch (e) {
-                console.log({error: e })
+        body('username').custom(async (username) => {
+            const existingUser = await User.findOne({ username })
+            if (existingUser) {
+                throw new Error('⚠️ User with the same username already exists!')
             }
         }),
-        body('email', '⚠️ User with the same email already exists!').custom(async (email) => {
-            try {
-                const existingEmail = await User.findOne({ email })
-                if (existingEmail) {
-                    throw new Error()
-                }
-            } catch (e) {
-                console.log({error: e})
+        body('email').custom(async (email) => {
+            const existingEmail = await User.findOne({ email })
+            if (existingEmail) {
+                throw new Error('⚠️ User with the same email already exists!')
             }
         }),
         (req, res, next) => {
